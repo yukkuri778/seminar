9afb42e558f415d2af0b42403370b8ed839a578c
kambak <kanba0531@gmail.com>
2025-12-31 13:15:27 +0900

    修正　discriminator

--- models/discriminator.py
+++ models/discriminator.py
@@ -15,13 +15,9 @@ class Discriminator(nn.Module):
         self.LeakyReLU = nn.LeakyReLU(0.2)
 
     def forward(self, input):
-        x = input.view(-1, 100)
+        x = input.view(-1, 784)
         x = self.LeakyReLU(self.fc1(x))
-        x = self.bn1(x)
         x = self.LeakyReLU(self.fc2(x))
-        x = self.bn2(x)
         x = self.LeakyReLU(self.fc3(x))
-        x = self.bn3(x)
-        x = torch.tanh(self.fc4(x))
-        x = x.view(-1, 28, 28)
+        x = torch.sigmoid(self.fc4(x))
         return x
