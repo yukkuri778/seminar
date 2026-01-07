a6f77acdd0930006f3e32b65bf506519cc08a594
yukkuri778 <yukkuri77789@gmail.com>
2026-01-07 22:04:14 +0900

    重み初期化関数をdata.pyで定義

--- models/generator.py
+++ models/generator.py
@@ -36,13 +36,4 @@ class Generator(nn.Module):
         )
 
     def forward(self, x):
-        return self.main(x)
-    
-    #重み初期化関数
-    def _weights_init(self, m):
-        classname = m.__class__.__name__
-        if classname.find('Conv') != -1:
-            nn.init.normal_(m.weight.data, 0.0, 0.02)
-        elif classname.find('BatchNorm') != -1:
-            nn.init.normal_(m.weight.data, 1.0, 0.02)
-            nn.init.constant_(m.bias.data, 0)
+        return self.main(x)    
